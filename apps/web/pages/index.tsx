152ffb07741c41ddb200e16d05150e16f54f1a5d
Richard Huf <richardhuf84@gmail.com>
2022-11-12 01:07:51 +1100

    fix: emotion

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,15 +1,17 @@
-import { Button, Canvas } from "ui";
-import { css } from '@emotion/react';
+import { Canvas } from "ui";
+import styled from '@emotion/styled';
+
+const StyledApp = styled('div')({ 
+  fontFamily: 'sans-serif'
+ })
 
 export default function Web() {
   return (
-    <div css={css`
-        font-family: sans-serif;
-      `}>
+    <StyledApp>
       <h1>
         Drawzo
       </h1>
       <Canvas />
-    </div>
+    </StyledApp>
   );
 }
